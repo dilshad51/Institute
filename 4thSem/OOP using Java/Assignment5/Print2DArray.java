d300fed890884df3f55c5e7a877d07278301d576
dilshad51 <dilshadara51@gmail.com>
2026-06-09 02:48:51 +0530

    Added new lab programs

---
+++ 4thSem/OOP using Java/Assignment5/Print2DArray.java
@@ -0,0 +1,17 @@
+// WAP in java to create a 2D array and take input statically and then print the elements in the 2D array in the matrix format
+public class Print2DArray {
+    public static void main(String[] args) {
+        int [][]matrix={
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("The 2D array :");
+        for (int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
