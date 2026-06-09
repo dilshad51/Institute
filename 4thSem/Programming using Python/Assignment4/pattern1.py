84c35364c0d3c0d42b688842393f8881b50274f4
dilshad51 <dilshadara51@gmail.com>
2026-06-10 00:55:55 +0530

    Added new lab programs

--- 4thSem/Programming using Python/Assignment4/pattern1.py
+++ 4thSem/Programming using Python/Assignment4/pattern1.py
@@ -1,3 +1,11 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+
+
 # n=int(input("Enter n: "))
 # for i in range(1,n+1):
 #     for j in range(1,i+1):
--- 4thSem/Programming using Python/Assignment4/pattern6.py
+++ 4thSem/Programming using Python/Assignment4/pattern6.py
@@ -1,3 +1,10 @@
+#      1 
+#     1 1 
+#    1 2 1 
+#   1 3 3 1 
+#  1 4 6 4 1 
+# 1 5 10 10 5 1
+
 l=int(input("Enter n:"))
 def ncr(n,r):
     return fact(n)//(fact(n-r)*fact(r))
