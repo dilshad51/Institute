0a6b63b56364f3e99aa1816b98eedd0b7e69fb98
dilshad51 <dilshadara51@gmail.com>
2026-06-11 01:28:47 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/10_area_circumference_circle.c
+++ 1stSem/Programming for problem solving(C language)/10_area_circumference_circle.c
@@ -1,1 +1,15 @@
 // Write a program in C to enter radius of circle and find the area and circumference.
+#include<stdio.h>
+int main(){
+    float r;
+    printf("Enter radius: ");
+    scanf("%f",&r);
+
+    float area=3.14*r*r;
+    float cir=2*3.14*r;
+
+    printf("Area: %.2f",area);
+    printf("\tCircumference: %.2f",cir);
+
+    return 0;
+}
