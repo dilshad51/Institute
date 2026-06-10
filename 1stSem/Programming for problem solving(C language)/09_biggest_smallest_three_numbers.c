2afdb8d79dd411ff9a3cfb73ec0a40d9f7a89575
dilshad51 <dilshadara51@gmail.com>
2026-06-11 01:27:00 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/09_biggest_smallest_three_numbers.c
+++ 1stSem/Programming for problem solving(C language)/09_biggest_smallest_three_numbers.c
@@ -1,1 +1,23 @@
 // Write a program in C to print 3 numbers and check the biggest and smallest.
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter 3 numbers: ");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b && a>c)
+        printf("Biggest number: %d\n",a);
+    else if(b>c)
+        printf("Biggest number: %d\n",b);
+    else
+        printf("Biggest number: %d\n",c);
+
+    if(a<b && a<c)
+        printf("Smallest number: %d",a);
+    else if(b<c)
+        printf("Smallest number: %d",b);
+    else
+        printf("Smallest number: %d",c);
+
+    return 0;
+}
