bae0439364652cf8054d3e4be9c601c5901f2da1
dilshad51 <dilshadara51@gmail.com>
2026-06-11 01:33:06 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/13_sum_series.c
+++ 1stSem/Programming for problem solving(C language)/13_sum_series.c
@@ -1,1 +1,19 @@
 // Write a program in C to find the forward series of a given number and its sum.
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Series: ");
+
+    for(int i=1;i<=n;i++){
+        printf("%d ",i);
+        sum=sum+i;
+    }
+
+    printf("\nSum=%d",sum);
+
+    return 0;
+}
