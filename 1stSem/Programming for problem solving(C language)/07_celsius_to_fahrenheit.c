c18714d2064e883affac67a6c42acae8302008c6
dilshad51 <dilshadara51@gmail.com>
2026-06-11 00:51:30 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/07_celsius_to_fahrenheit.c
+++ 1stSem/Programming for problem solving(C language)/07_celsius_to_fahrenheit.c
@@ -1,1 +1,10 @@
 // Write a program in C to enter a temp in Celsius and turn it into Fahrenheit.
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("Enter temperature in Celcius: ");
+    scanf("%f",&c);
+    f=(c*1.8)+32;
+    printf("Temeprature in fahrenheit: %.2f",f);
+    return 0;
+}
--- 1stSem/Programming for problem solving(C language)/08_calculator_dynamic_input.c
+++ 1stSem/Programming for problem solving(C language)/08_calculator_dynamic_input.c
@@ -1,1 +1,6 @@
 // Write a program in C to print calculator of two numbers using dynamic input.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter ")
+}
