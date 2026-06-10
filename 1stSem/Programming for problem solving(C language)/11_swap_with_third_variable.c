1f0d142647561d9bca5319b90e9c13d626d86970
dilshad51 <dilshadara51@gmail.com>
2026-06-11 01:30:37 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/11_swap_with_third_variable.c
+++ 1stSem/Programming for problem solving(C language)/11_swap_with_third_variable.c
@@ -1,1 +1,21 @@
 // Write a program in C to swap the variable value using third variable.
+#include<stdio.h>
+int main(){
+    int a,b,temp;
+
+    printf("Enter first number: ");
+    scanf("%d",&a);
+
+    printf("Enter second number: ");
+    scanf("%d",&b);
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("After swapping:\n");
+    printf("a=%d",a);
+    printf("\tb=%d",b);
+
+    return 0;
+}
