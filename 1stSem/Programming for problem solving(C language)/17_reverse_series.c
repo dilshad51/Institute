578683317f9cec2e0f7fcc432a083a1eff8203a1
dilshad51 <dilshadara51@gmail.com>
2026-06-12 00:20:18 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/17_reverse_series.c
+++ 1stSem/Programming for problem solving(C language)/17_reverse_series.c
@@ -1,1 +1,16 @@
 // Write a program in C to find out the reverse series.
+#include<stdio.h>
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Reverse series: \n");
+
+    for(int i=n;i>=1;i--){
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
