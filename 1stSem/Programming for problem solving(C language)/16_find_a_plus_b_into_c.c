1d8b9720c8910c96d7c056df6ffdc8cc929eb8e6
dilshad51 <dilshadara51@gmail.com>
2026-06-12 00:18:37 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/16_find_a_plus_b_into_c.c
+++ 1stSem/Programming for problem solving(C language)/16_find_a_plus_b_into_c.c
@@ -1,1 +1,21 @@
 // Write a program in C to find A + B * C, where A, B are int and C is float.
+#include<stdio.h>
+int main(){
+    int a,b;
+    float c;
+
+    printf("Enter A: ");
+    scanf("%d",&a);
+
+    printf("Enter B: ");
+    scanf("%d",&b);
+
+    printf("Enter C: ");
+    scanf("%f",&c);
+
+    float result = a + b*c;
+
+    printf("Result: %.2f",result);
+
+    return 0;
+}
