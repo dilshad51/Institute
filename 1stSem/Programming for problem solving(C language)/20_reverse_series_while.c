77a4bcd3fdc132b1c29bffaea2de1766679f10e1
dilshad51 <dilshadara51@gmail.com>
2026-06-12 00:29:28 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/20_reverse_series_while.c
+++ 1stSem/Programming for problem solving(C language)/20_reverse_series_while.c
@@ -1,1 +1,17 @@
 // Write a program in C to find out reverse series using while loop.
+#include<stdio.h>
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Reverse series: ");
+
+    while(n>=1){
+        printf("%d ",n);
+        n--;
+    }
+
+    return 0;
+}
