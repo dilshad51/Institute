ca57d0635fc516695dcfebaea3a516cef11c51b4
dilshad51 <dilshadara51@gmail.com>
2026-06-13 00:31:52 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/23_forward_series_do_while.c
+++ 1stSem/Programming for problem solving(C language)/23_forward_series_do_while.c
@@ -1,1 +1,17 @@
 // Write a program in C to print the forward series using do while loop.
+#include<stdio.h>
+int main(){
+    int n,i=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Forward series: ");
+
+    do{
+        printf("%d ",i);
+        i++;
+    }while(i<=n);
+
+    return 0;
+}
