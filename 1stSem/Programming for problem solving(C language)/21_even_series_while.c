a7f34aee54cabb51765c41705328773c4512df33
dilshad51 <dilshadara51@gmail.com>
2026-06-13 00:29:37 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/21_even_series_while.c
+++ 1stSem/Programming for problem solving(C language)/21_even_series_while.c
@@ -1,1 +1,17 @@
 // Write a program in C to find the even series using while loop.
+#include<stdio.h>
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Even series: ");
+    int i=2;
+    while(i<=2*n){
+        printf("%d ",i);
+        i=i+2;
+    }
+
+    return 0;
+}
