da1ac8fc220221d503235f96f2df9ab0a87d347f
dilshad51 <dilshadara51@gmail.com>
2026-06-13 00:30:33 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/22_odd_series_while.c
+++ 1stSem/Programming for problem solving(C language)/22_odd_series_while.c
@@ -1,1 +1,17 @@
 // Write a program in C to print the odd series using while loop.
+#include<stdio.h>
+int main(){
+    int n,i=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Odd series: ");
+
+    while(i<=n){
+        printf("%d ",i);
+        i=i+2;
+    }
+
+    return 0;
+}
