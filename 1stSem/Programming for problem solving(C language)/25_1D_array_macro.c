24cd1a98a22b874796e710d71668eaf9f88fe2a0
dilshad51 <dilshadara51@gmail.com>
2026-06-13 00:33:42 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/25_1D_array_macro.c
+++ 1stSem/Programming for problem solving(C language)/25_1D_array_macro.c
@@ -1,1 +1,19 @@
 // Write a program in C to print 1D array using macro.
+#include<stdio.h>
+#define SIZE 5
+
+int main(){
+    int a[SIZE];
+
+    printf("Enter %d elements: ",SIZE);
+
+    for(int i=0;i<SIZE;i++)
+        scanf("%d",&a[i]);
+
+    printf("Array elements: ");
+
+    for(int i=0;i<SIZE;i++)
+        printf("%d ",a[i]);
+
+    return 0;
+}
