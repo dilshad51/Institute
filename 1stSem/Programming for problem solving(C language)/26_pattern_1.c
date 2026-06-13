bc8b21ff1b2accc2e3b34d691a3e0b1f2c2a2f11
dilshad51 <dilshadara51@gmail.com>
2026-06-14 03:13:58 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/26_pattern_1.c
+++ 1stSem/Programming for problem solving(C language)/26_pattern_1.c
@@ -1,1 +1,17 @@
-// Write a program in C to print following pattern.
+// // Write a program in C to print following pattern.
+// *
+// **
+// ***
+// ****
+#include<stdio.h>
+int main(){
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
--- 1stSem/Programming for problem solving(C language)/27_pattern_2.c
+++ 1stSem/Programming for problem solving(C language)/27_pattern_2.c
@@ -1,1 +1,5 @@
 // Write a program in C to print the pattern.
+// 1
+// 12
+// 123
+// 1234
