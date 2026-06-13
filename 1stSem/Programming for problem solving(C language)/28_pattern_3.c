98aefb2eac4fcaa404d56ef9dede67f04c8facf3
dilshad51 <dilshadara51@gmail.com>
2026-06-14 03:15:43 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/28_pattern_3.c
+++ 1stSem/Programming for problem solving(C language)/28_pattern_3.c
@@ -1,1 +1,20 @@
 // Write a program in C to print the pattern as follows.
+//    A
+//   AB
+//  ABC
+// ABCD
+#include<stdio.h>
+int main(){
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4-i;j++)
+            printf(" ");
+
+        for(char k='A';k<'A'+i;k++)
+            printf("%c",k);
+
+        printf("\n");
+    }
+
+    return 0;
+}
