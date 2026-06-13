6e3c679d11e661b01f0a2d5520c60f1e1265492d
dilshad51 <dilshadara51@gmail.com>
2026-06-14 03:14:18 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/27_pattern_2.c
+++ 1stSem/Programming for problem solving(C language)/27_pattern_2.c
@@ -3,3 +3,15 @@
 // 12
 // 123
 // 1234
+#include<stdio.h>
+int main(){
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
