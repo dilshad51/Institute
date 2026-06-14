89da4fd3da2f4d47b9169505314bfedcdc4f2e95
dilshad51 <dilshadara51@gmail.com>
2026-06-15 00:32:42 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/33_factorial_do_while.c
+++ 1stSem/Programming for problem solving(C language)/33_factorial_do_while.c
@@ -1,1 +1,17 @@
 // Write a program in C to print factorial of a number using do while loop.
+#include<stdio.h>
+int main(){
+    int n,i=1,fact=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    do{
+        fact=fact*i;
+        i++;
+    }while(i<=n);
+
+    printf("Factorial=%d",fact);
+
+    return 0;
+}
