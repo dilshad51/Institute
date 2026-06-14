e9339983d97c9eebde6a443f0258427891626469
dilshad51 <dilshadara51@gmail.com>
2026-06-15 01:59:52 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/36_pointer_value.c
+++ 1stSem/Programming for problem solving(C language)/36_pointer_value.c
@@ -1,1 +1,16 @@
 // Write a program in C to print the value using pointer.
+#include<stdio.h>
+int main(){
+    int a;
+    int *p;
+
+    printf("Enter a number: ");
+    scanf("%d",&a);
+
+    p=&a;
+
+    printf("Value=%d\n",*p);
+    printf("Address=%p",p);
+
+    return 0;
+}
