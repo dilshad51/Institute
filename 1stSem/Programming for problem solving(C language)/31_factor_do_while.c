548fbad667d7d9f7d43be1cf33a08b4c4a366961
dilshad51 <dilshadara51@gmail.com>
2026-06-14 18:58:19 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/31_factor_do_while.c
+++ 1stSem/Programming for problem solving(C language)/31_factor_do_while.c
@@ -1,1 +1,19 @@
 // Write a program in C to print the factor of a number using do while loop.
+#include<stdio.h>
+int main(){
+    int n,i=1;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    printf("Factors: ");
+
+    do{
+        if(n%i==0)
+            printf("%d ",i);
+
+        i++;
+    }while(i<=n);
+
+    return 0;
+}
