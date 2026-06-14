304f94575b7073a5c1101f9fa3da5badeaa027f5
dilshad51 <dilshadara51@gmail.com>
2026-06-15 02:04:59 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/37_sum_using_pointer.c
+++ 1stSem/Programming for problem solving(C language)/37_sum_using_pointer.c
@@ -1,1 +1,16 @@
 // Write a program in C to print the summation of two variables using pointer.
+#include<stdio.h>
+int main(){
+    int a,b;
+    int *p,*q;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    p=&a;
+    q=&b;
+
+    printf("Sum=%d",*p+*q);
+
+    return 0;
+}
