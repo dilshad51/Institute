2434d14b803c677e24965b4a3be47646826e3cff
dilshad51 <dilshadara51@gmail.com>
2026-06-16 01:36:22 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/38_chain_of_pointer.c
+++ 1stSem/Programming for problem solving(C language)/38_chain_of_pointer.c
@@ -1,1 +1,16 @@
 // Write a program in C for chain of pointer.
+#include<stdio.h>
+int main(){
+    int a=10;
+    int *p;
+    int **q;
+
+    p=&a;
+    q=&p;
+
+    printf("Value of a=%d\n",a);
+    printf("Using p=%d\n",*p);
+    printf("Using q=%d",**q);
+
+    return 0;
+}
