c4bba97ef3b495e063c5658789f8f3a2c3eb16b5
dilshad51 <dilshadara51@gmail.com>
2026-06-16 01:37:07 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/39_array_of_pointer.c
+++ 1stSem/Programming for problem solving(C language)/39_array_of_pointer.c
@@ -1,1 +1,16 @@
 // Write a program in C to print array of pointer.
+#include<stdio.h>
+int main(){
+    int a[5]={10,20,30,40,50};
+    int *p[5];
+
+    for(int i=0;i<5;i++)
+        p[i]=&a[i];
+
+    printf("Array elements using pointers:\n");
+
+    for(int i=0;i<5;i++)
+        printf("%d ",*p[i]);
+
+    return 0;
+}
