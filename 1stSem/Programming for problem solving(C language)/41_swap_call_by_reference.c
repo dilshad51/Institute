efb23735b3a51524bf528081fea1d4c8e1485795
dilshad51 <dilshadara51@gmail.com>
2026-06-16 01:38:25 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/41_swap_call_by_reference.c
+++ 1stSem/Programming for problem solving(C language)/41_swap_call_by_reference.c
@@ -1,1 +1,24 @@
 // Write a program in C to swap two numbers using call by reference.
+#include<stdio.h>
+
+void swap(int *a,int *b){
+    int temp;
+
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int main(){
+    int a,b;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    swap(&a,&b);
+
+    printf("After swap:\n");
+    printf("a=%d\tb=%d",a,b);
+
+    return 0;
+}
