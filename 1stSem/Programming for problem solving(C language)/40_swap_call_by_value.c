a7669b9ca1c97cc12746611af9a08fc7b0a502f7
dilshad51 <dilshadara51@gmail.com>
2026-06-16 01:37:55 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/40_swap_call_by_value.c
+++ 1stSem/Programming for problem solving(C language)/40_swap_call_by_value.c
@@ -1,1 +1,26 @@
 // Write a program in C to swap two numbers using call by value.
+#include<stdio.h>
+
+void swap(int a,int b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("Inside function:\n");
+    printf("a=%d\tb=%d\n",a,b);
+}
+
+int main(){
+    int a,b;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    swap(a,b);
+
+    printf("Outside function:\n");
+    printf("a=%d\tb=%d",a,b);
+
+    return 0;
+}
