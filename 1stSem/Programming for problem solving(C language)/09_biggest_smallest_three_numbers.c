93c95e783d48fa7c90de6bff6c52536f3b37615f
dilshad51 <dilshadara51@gmail.com>
2026-06-30 22:02:20 +0530

    Added new lab programs

--- 1stSem/Programming for problem solving(C language)/09_biggest_smallest_three_numbers.c
+++ 1stSem/Programming for problem solving(C language)/09_biggest_smallest_three_numbers.c
@@ -5,7 +5,7 @@ int main(){
     printf("Enter 3 numbers: ");
     scanf("%d%d%d",&a,&b,&c);
 
-    if(a>b && a>c)
+    if(a<b && a>c)
         printf("Biggest number: %d\n",a);
     else if(b>c)
         printf("Biggest number: %d\n",b);
