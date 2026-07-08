ce35bdee650ced5227b03701c40a57e7dce94436
dilshad51 <dilshadara51@gmail.com>
2026-07-08 23:41:44 +0530

    Added new lab programs

--- 3rdSem/C++ Lab/01_Arithmetic_Using_Class_Object.cpp
+++ 3rdSem/C++ Lab/01_Arithmetic_Using_Class_Object.cpp
@@ -1,3 +1,44 @@
 // Experiment 1
 // Add, Subtract, Multiply, Divide and Modulus of two numbers using class and object.
 #include<iostream>
+using namespace std;
+class Adder{
+    public:
+    int num1,num2;
+    void inputNumbers(){
+        cout<<"Enter first number: ";
+        cin>>num1;
+        cout<<"Enter second number: ";
+        cin>>num2;
+    }
+    void addAndDisplay(){
+        int sum=num1+num2;
+        cout<<"Sum="<<sum<<endl;
+    }
+    void subAndDisplay(){
+        int sub=num1-num2;
+        cout<<"Difference="<<sub<<endl;
+    }
+    void mulAndDisplay(){
+        int pro=num1*num2;
+        cout<<"Product="<<pro<<endl;
+    }
+    void divAndDisplay(){
+        int div=num1/num2;
+        cout<<"Division="<<div<<endl;
+    }
+    void modAndDisplay(){
+        int mod=num1%num2;
+        cout<<"Modulus="<<mod<<endl;
+    }
+};
+int main(){
+        Adder obj;
+        obj.inputNumbers();
+        obj.addAndDisplay();
+        obj.subAndDisplay();
+        obj.mulAndDisplay();
+        obj.divAndDisplay();
+        obj.modAndDisplay();
+        return 0;
+    }
