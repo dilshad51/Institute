2e604ed65ae06e58c701f0b31b2761fbad705e89
dilshad51 <dilshadara51@gmail.com>
2026-07-09 22:40:54 +0530

    Added new lab programs

--- 3rdSem/C++ Lab/02_Default_And_Parameterized_Constructor.cpp
+++ 3rdSem/C++ Lab/02_Default_And_Parameterized_Constructor.cpp
@@ -1,2 +1,39 @@
 // Experiment 2
 // WAP in C++ using a class named MyTest that demonstrates both a default constructor and a parameterized constructor.
+#include <iostream>
+using namespace std;
+
+class MyTest {
+private:
+    int num;
+
+public:
+    // Default Constructor
+    MyTest() {
+        num = 0;
+        cout << "Default Constructor Called" << endl;
+    }
+
+    // Parameterized Constructor
+    MyTest(int n) {
+        num = n;
+        cout << "Parameterized Constructor Called" << endl;
+    }
+
+    // Display function
+    void display() {
+        cout << "Value of num = " << num << endl;
+    }
+};
+
+int main() {
+    // Calls default constructor
+    MyTest obj1;
+    obj1.display();
+
+    // Calls parameterized constructor
+    MyTest obj2(100);
+    obj2.display();
+
+    return 0;
+}
