2de924650c9a9c37e8908ac016c048b2ed7fc2a5
dilshad51 <dilshadara51@gmail.com>
2026-07-10 23:26:28 +0530

    Added new lab programs

--- 3rdSem/C++ Lab/03_Default_Constructor.cpp
+++ 3rdSem/C++ Lab/03_Default_Constructor.cpp
@@ -1,2 +1,14 @@
 // Experiment 3
 // Default Constructor
+#include<iostream>
+using namespace std;
+class Example{
+    public:
+        Example(){
+            cout<<"Default Constructor called"<<endl;
+        }
+};
+int main(){
+    Example obj;
+    return 0;
+}
